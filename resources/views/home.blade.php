0271fabeeeb972dc9efb6fd0523728cac0fbbb2c
Kakon Ray <thisiskakonray@gmail.com>
2022-09-10 14:48:10 +0600

    master layout and home page setup

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,20 @@
-home
+@extends('layout.app')
+
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-lg-3">
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Lorem ipsum, dolor sit amet consectetur adipisicing elit. </p>
+        </div>
+        <div class="col-lg-3">
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Lorem ipsum, dolor sit amet consectetur adipisicing elit. </p>
+        </div>
+        <div class="col-lg-3">
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Lorem ipsum, dolor sit amet consectetur adipisicing elit. </p>
+        </div>
+   
+    </div>
+</div>
+
+@endsection('content')
---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel Website</title>
+     <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+     <link rel="stylesheet" href="{{asset('css/custome.css')}}">
+</head>
+<body>
+    
+@include('layout.menue')
+
+@yield('content')
+
+@include('layout.footer')
+
+
+<script src="{{asset('js/jquery-3.6.1.min.js')}}"></script>
+<script src="{{asset('js/popper.min')}}"></script>
+<script src="{{asset('js/bootstrap.min.js')}}"></script>     
+<script src="{{asset('js/custome.js')}}"></script>
+
+</body>
+</html>
